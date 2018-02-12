cb42526fc56071b58772e65329fd2867c00364cc
Canna Wen <cannawen@gmail.com>
2018-02-12 10:48:00 +0800

    Remove unnecessary UI elements. Ask for storage permissions on install

--- app/src/main/java/com/cannawen/moodtracker/MainActivity.java
+++ app/src/main/java/com/cannawen/moodtracker/MainActivity.java
@@ -1,20 +1,20 @@
 package com.cannawen.moodtracker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+    private static final int CODE_EXTERNAL_STORAGE_PERMISSION = 1024;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,83 +24,69 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        if (!Settings.canDrawOverlays(this)) {
-            //If the draw over permission is not available open the settings screen
-            //to grant the permission.
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
-        } else {
-            initializeView();
-        }
+        initialize();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        startService(new Intent(MainActivity.this, RatingViewService.class));
+        showMoodRatingView();
     }
 
-    /**
-     * Set and initialize the view elements.
-     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            initialize();
+        }
+    }
+
+    private void initialize() {
+        if (!hasStoragePermission()) {
+            requestStoragePermission();
+        } else if (!hasDrawOverlayPermission()) {
+            requestDrawOverlayPermission();
+        } else {
+            initializeView();
+        }
+    }
+
+    private boolean hasStoragePermission() {
+        return checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, CODE_EXTERNAL_STORAGE_PERMISSION);
+    }
+
+    private boolean hasDrawOverlayPermission() {
+        return Settings.canDrawOverlays(this);
+    }
+
+    private void requestDrawOverlayPermission() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+    }
+
     private void initializeView() {
-        findViewById(R.id.notify_me).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.rate_mood_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startService(new Intent(MainActivity.this, RatingViewService.class));
-//                finish();
+                showMoodRatingView();
             }
         });
     }
 
+    private void showMoodRatingView() {
+        startService(new Intent(MainActivity.this, RatingViewService.class));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
-
-            //Check if the permission is granted or not.
-            if (resultCode == RESULT_OK) {
-                initializeView();
-            } else { //Permission is not available
-                Toast.makeText(this,
-                        "Draw over other app permission not available. Closing the application",
-                        Toast.LENGTH_SHORT).show();
-
-                finish();
-            }
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION && resultCode == RESULT_OK) {
+            initialize();
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
